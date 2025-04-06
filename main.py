ac3e7822b0d5279439d983b4eff27b8b0d8a2746
João Baptista <joao.baptista@outlook.pt>
2025-04-06 01:09:49 +0100

    start route dev

--- main.py
+++ main.py
@@ -10,3 +10,33 @@ APP = Flask(__name__)
 @APP.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@APP.route('/profile')
+def profile():
+    return render_template('underdev.html')
+
+
+
+@APP.route('/virtualmachine')
+def vm():
+    return render_template('underdev.html')
+
+
+
+@APP.route('/diskstorage')
+def diskstorage():
+    return render_template('underdev.html')
+
+
+
+@APP.route('/container')
+def containers():
+    return render_template('underdev.html')
+
+
+
+@APP.route('/database')
+def databased():
+    return render_template('underdev.html')
